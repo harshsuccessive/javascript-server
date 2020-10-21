610b0cd020e0955064d9da962202b83b2f5865cb
Harsh Rastogi <harsh.rastogi@tr23.successivetech.com>
2020-10-21 16:36:18 +0530

    Day4 Object&Array

---
+++ extra/utils/validation.js
@@ -0,0 +1,48 @@
+const user =[{
+    traineeEmail: 'trainee1@successive.tech',
+    reviewerEmail: 'reviewer1@successive.tech',
+    },{
+        traineeEmail: 'trainee1@succssive.tech',
+        reviewerEmail: 'reviewer1@successive.tech',
+        }];
+
+   function validateemail(email)
+   {
+       let regx =/^([a-zA-z0-9/.-]+)@(successive).(tech)$/
+        return regx.test(email);
+   }
+
+   function validateuser(user)
+   {
+       let valid=0;
+       let invalid =0;
+       user.forEach(element => {
+           const {traineeEmail}= element;
+           const {reviewerEmail}= element;
+           if(validateemail(traineeEmail)==true)
+           {
+               valid=valid+1;
+               console.log("valid user = ",traineeEmail)
+           }
+           else
+           {
+            console.log("invalid user = ",traineeEmail)
+               invalid=invalid+1;
+           }
+           if(validateemail(reviewerEmail)==true)
+           {
+            console.log("valid user = ",reviewerEmail)
+            valid=valid+1;
+           }
+           else
+           {
+            console.log("invalid user = ",reviewerEmail)
+               invalid=invalid+1;
+           }
+       });
+       console.log("Count of valid users = ",valid);
+       console.log("count of invalid users = ",invalid);
+   }
+
+
+   validateuser(user);
