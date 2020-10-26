9954d6a4a41b359ea61bed0ee23e78bfabb63159
harshsuccessive <harsh.rastogi@successive.tech>
2020-10-26 17:41:18 +0530

    moved babelrc

--- extra/patterns/diamond.js
+++ extra/patterns/diamond.js
@@ -1,38 +1,32 @@
-let x,a
+let a;
 // n=process.argv[2];
 // diamond(n);
 
-export default function diamond(x)
-{
-    console.log("print a diamond with rows "+x);
-    
-    if( x<2 && x>10 )
-       return(0);
-
-    for(let i=0; i<x; i++)
-     { 
-        a="";
-
-    for(let j=i; j<x-1; j++)
-        a+=" ";
-
-    for (let z=i; z>=0; z--)
-        a+="* ";
+export default function diamond(x) {
+    console.log('print a diamond with rows ' + x);
+    if ( x < 2 && x > 10 ) {
+       console.log('enter the value between 2 to 10');
+    }
+    else {
+    for ( let i = 0; i < x; i++ ) {
+        a = '';
+    for ( let j = i; j < x - 1 ; j++ )
+        a += '';
+    for ( let z = i; z >= 0; z-- )
+        a += '* ';
 
     console.log(a);
 
      }
-    for(let i=0; i<x; i++)
-     {
-          a="";
+    for ( let i = 0; i < x; i++ ) {
+          a = '';
 
-    for(let j=i; j>0; j--)
-        a+=" ";
+    for ( let j = i; j > 0; j-- )
+        a += ' ';
 
-    for(let z=i; z<x; z++)
-        a+="* ";
-       
+    for ( let z = i; z < x; z++ )
+        a += '* ';
      console.log(a);
 
      }
-}
+}}
