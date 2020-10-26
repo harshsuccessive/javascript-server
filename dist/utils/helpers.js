1d89aa4a91e47ddbce2fbe3c6f86cd190938994b
harshsuccessive <harsh.rastogi@successive.tech>
2020-10-26 13:59:06 +0530

    Resolved all Issues

--- dist/utils/helpers.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.validateEmail = void 0;
-function validateEmail(email) {
-    let regx = /^([a-zA-z0-9/.-]+)@(successive).(tech)$/;
-    return regx.test(email);
-}
-exports.validateEmail = validateEmail;
-//# sourceMappingURL=helpers.js.map
