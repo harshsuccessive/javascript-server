3423e10e708f7ea602eaf68bc8b66e2367946e67
Harsh Rastogi <72728668+harshsuccessive@users.noreply.github.com>
2020-10-26 18:23:58 +0530

    Update diamond.js

--- extra/patterns/diamond.js
+++ extra/patterns/diamond.js
@@ -12,7 +12,7 @@ export default function diamond(x) {
     for ( let i = 0; i < x; i++ ) {
         a = '';
     for ( let j = i; j < x - 1 ; j++ )
-        a += '';
+        a += ' ';
     for ( let z = i; z >= 0; z-- )
         a += '* ';
 =======
