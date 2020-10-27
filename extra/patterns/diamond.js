d720b406af674c0e1f62c65f03c2456a8cacff9f
harshsuccessive <harsh.rastogi@successive.tech>
2020-10-27 09:10:00 +0530

    Resolved Conflicts

--- extra/patterns/diamond.js
+++ extra/patterns/diamond.js
@@ -2,7 +2,6 @@ let a;
 // n=process.argv[2];
 // diamond(n);
 
-<<<<<<< HEAD
 export default function diamond(x) {
     console.log('print a diamond with rows ' + x);
     if ( x < 2 && x > 10 ) {
@@ -12,29 +11,9 @@ export default function diamond(x) {
     for ( let i = 0; i < x; i++ ) {
         a = '';
     for ( let j = i; j < x - 1 ; j++ )
-        a += '';
+        a += ' ';
     for ( let z = i; z >= 0; z-- )
         a += '* ';
-=======
-export default function diamond(x)
-{
-    console.log("print a diamond with rows "+x);
-    
-    if( x<2 && x>10 ){
-       console.log("enter the value between 2 to 10");
-    }
-    else{
-
-    for(let i=0; i<x; i++)
-     { 
-        a="";
-
-    for(let j=i; j<x-1; j++)
-        a+=" ";
-
-    for (let z=i; z>=0; z--)
-        a+="* ";
->>>>>>> 8e5ec29b98f4ac62767f62c4e2deb8cabd179f3c
 
     console.log(a);
 
--- extra/patterns/equilateral.js
+++ extra/patterns/equilateral.js
@@ -1,7 +1,6 @@
 let a
 // n=process.argv[2]
 // equilateral(n)
-<<<<<<< HEAD
 export default function equilateral (x) {
     console.log('print a equilateral triangle with rows ' + x);
     if ( x < 2 && x > 10 ) {
@@ -10,20 +9,6 @@ export default function equilateral (x) {
     else {
     for ( let i = 1; i <= x; i++ ) {
           a = '';
-=======
-export default function equilateral (x)
-{
-    console.log("print a equilateral triangle with rows "+x);
-   
-    if(x<2 && x>10){
-    console.log("enter the value between 2 to 10");
-    }
-    else{
-   
-    for(let i=1;i<=x;i++)
-     {
-          a="";
->>>>>>> 8e5ec29b98f4ac62767f62c4e2deb8cabd179f3c
 
     for ( let j = i; j <= x; j++ ) {
           a += ' ';
