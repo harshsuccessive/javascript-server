3d5a45d56e986c1fd88ceae7d30a2146b2c86ff3
harshsuccessive <harsh.rastogi@successive.tech>
2020-10-28 13:27:28 +0530

    HTTP STATUS and Handler

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,51 @@
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
+
+import { notFoundHandler, errorHandler } from './libs/routes';
+
+
 class Server{
     app
     constructor(private config){
         this.app = express()
     }
+
     bootstrap(){
         this.setupRoutes();
+        this.initBodyParser();
         return this;
     }
+
     setupRoutes(){
+
+
         const { app } = this;
-        app.get('/health-check',(req, res, next) =>{
+
+
+
+        this.app.get('/health-check',(req, res, next) =>{
+            console.log("Inside second middleware");
             res.send('I am Ok');
-        })
+        });
+
+
+
+        this.app.use(notFoundHandler);
+
+        this.app.use(errorHandler);
+
+
         return this;
     }
+
+
+    public initBodyParser(){
+        this.app.use(bodyParser.json({ type: 'application/*+json' }));
+
+    }
+
+
+
     run(){
         const {app, config :{port}} = this;
         app.listen(port,(err) =>{
