508cf4726d2d77b7050b33aa103eab6703f2085c
harshsuccessive <harsh.rastogi@successive.tech>
2020-11-27 11:32:47 +0530

    updates

--- src/libs/validationHandler.ts
+++ src/libs/validationHandler.ts
@@ -1,103 +1,59 @@
-// import { NextFunction, Request, Response } from "express";
-
-// export default (config) => (req: Request, res: Response, next: NextFunction) =>{
-//     console.log("config", config);
-//     console.log(req.query);
-//     console.log(req.body);
-//     next();
-// }
-
-
-import { Request, Response, NextFunction } from 'express';
-function checkRegex(stringtovalidate: string, regex: RegExp): boolean {
-    return regex.test(stringtovalidate);
-}
-export const validationHandler = ( config ) => (req: Request, res: Response, next: NextFunction) => {
-    const keys = Object.keys(config);
-    const body = Object.keys(req.body).length;
-    const params = Object.keys(req.query).length;
-    const query = Object.keys(req.query).length;
-    keys.forEach((key) => {
-        const item = config[key];
-        const value = item.in.map((val) => {
-            return req[val][key];
+import { NextFunction, Request, Response } from 'express';
+export const validationHandler = ( config ) => ( req: Request, res: Response, next: NextFunction  ) => {
+    const error = [];
+    Object.keys(config).forEach((keys) => {
+        const inObject = config[keys];
+        inObject.in.forEach(inside => {
+            let value = req[inside][keys];
+            const temp = {
+                key : '',
+                location: '',
+                errorMessage: ''
+            };
+            if ((inObject.required) && !(value)) {
+                temp.key = keys;
+                temp.location = inside;
+                temp.errorMessage = inObject.errorMessage || `${keys} is required`;
+                error.push(temp);
+                return;
+            }
+            value = value || inObject.default;
+            if (!value) {
+                return;
+            }
+            if ((inObject.number) && !(Number.isInteger(Number(value)))) {
+                temp.key = keys;
+                temp.location = inside;
+                temp.errorMessage = inObject.errorMessage || `${keys}'s type is not number`;
+                error.push(temp);
+                return;
+            }
+            if ((inObject.string) && !(typeof value === 'string')) {
+                temp.key = keys;
+                temp.location = inside;
+                temp.errorMessage = inObject.errorMessage || `${keys}'s type is not string`;
+                error.push(temp);
+                return;
+            }
+            const regex = inObject.regex;
+            if ((regex) && !regex.test(value)) {
+                temp.key = keys;
+                temp.location = inside;
+                temp.errorMessage = inObject.errorMessage || `${keys} is invalid`;
+                error.push(temp);
+                return;
+            }
+            if (inObject.isObject && (!(typeof value === 'object') || !(Object.entries(value).length))) {
+                temp.key = keys;
+                temp.location = inside;
+                temp.errorMessage = `${keys} is invalid`;
+                error.push(temp);
+                return;
+            }
         });
-        console.log(value);
-        console.log(req.method);
-        if ( item && item.required ) {
-            if ( req.method === 'POST' || req.method === 'PUT' ) {
-                if ( body !== keys.length ) {
-                    next({
-                        error: item.errorMessage,
-                        message: 'data is missing ',
-                        status: 404
-                    });
-                }
-            }
-            if ( req.method === 'GET' ) {
-                if (query !== keys.length) {
-                    next({
-                        error: item.errorMessage,
-                        message: 'data is missing ',
-                        status: 404
-                    });
-                }
-            }
-            if ( req.method === 'DELETE' ) {
-                if ( params !== keys.length ) {
-                    console.log(keys.length);
-                    next({
-                        error: item.errorMessage,
-                        message: 'data is missing ',
-                        status: 404
-                    });
-                }
-            }
-            if ( req.method === 'DELETE' ) {
-                next({
-                    error: item.errorMessage,
-                    message: 'Id is required',
-                    status: 404
-                });
-            }
-            if ( item.string ) {
-                if (!('string' === typeof value[0])) {
-                    next({
-                        error: item.errorMessage,
-                        message: `${key} is not string type`,
-                        status: 400,
-                    });
-                }
-            }
-            if ( item.number ) {
-                if ( isNaN(value[0]) ) {
-                    next({
-                        error: item.errorMessage,
-                        message: `${key} is not number type`,
-                        staus: 400,
-                    });
-                }
-            }
-            if ( item.regex ) {
-                const flag = checkRegex(value[0], /^[A-Za-z]+$/);
-                console.log(flag);
-                if (!flag) {
-                    next({
-                        error: item.errorMessage,
-                        message: 'name can only consist of alphabets ' ,
-                        status: 400,
-                    });
-                }
-            }
-        }
-        else if (!item.required) {
-            if (item.number) {
-                if (value[0] === '') {
-                    console.log(key + ' = ', item.default);
-                }
-            }
-        }
     });
-    next();
-
+    if (error.length) {
+        return res.status(400).send(error);
+    }
+    next ();
 };
